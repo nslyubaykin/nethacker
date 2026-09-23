d959ef2321946757e551fdb5583c9a14c17b6cb3
nslyubaykin <nslyubaykin@yandex.ru>
2026-09-23 03:37:27 +0300

    nethackers evolve win

--- autoascend/agent.py
+++ autoascend/agent.py
@@ -1416,9 +1416,16 @@ class Agent:
 
         items = [item for item in flatten_items(self.inventory.items) if item.is_unambiguous() and
                  item.category == nh.POTION_CLASS and item.object.name in ['healing', 'extra healing', 'full healing']]
+        # hypothesis: using a known healing potion while an enemy is already adjacent
+        # prevents low-HP melee exchanges from becoming sudden deaths across roles.
+        adjacent_enemy = any(
+            utils.adjacent((self.blstats.y, self.blstats.x), (y, x))
+            for _, y, x, _, _ in self.get_visible_monsters()
+        )
         if (
                 (self.blstats.hitpoints < 1 / 3 * self.blstats.max_hitpoints
-                 or self.blstats.hitpoints < 8) and items
+                 or self.blstats.hitpoints < 8
+                 or (adjacent_enemy and self.blstats.hitpoints < 1 / 2 * self.blstats.max_hitpoints)) and items
         ):
             yield True
             self.inventory.quaff(items[0])
@@ -1458,14 +1465,18 @@ class Agent:
     def eat_from_inventory(self):
         if self.blstats.hunger_state < Hunger.HUNGRY:
             yield False
-        for item in flatten_items(self.inventory.items):
-            if item.category == nh.FOOD_CLASS and \
-                    item.objs[0].name != 'sprig of wolfsbane' and \
-                    (not item.is_corpse() or
-                     item.monster_id in [MON.from_name(n) - nh.GLYPH_MON_OFF for n in ['lizard', 'lichen']]):
-                yield True
-                self.inventory.eat(item)
-                return
+        # hypothesis: choosing the most nutrition-efficient safe food first
+        # shortens hungry interruptions and prevents food supplies being spent
+        # on low-value snacks before they can avert starvation.
+        food = [item for item in flatten_items(self.inventory.items)
+                if item.category == nh.FOOD_CLASS and
+                item.objs[0].name != 'sprig of wolfsbane' and
+                (not item.is_corpse() or
+                 item.monster_id in [MON.from_name(n) - nh.GLYPH_MON_OFF for n in ['lizard', 'lichen']])]
+        for item in sorted(food, key=lambda item: item.object.nutrition / max(item.object.delay, 1), reverse=True):
+            yield True
+            self.inventory.eat(item)
+            return
         yield False
 
     @utils.debug_log('cure_disease')
